588a458a1fc10c76be391a9830bd771027c1f03e
Radisa Jovicevic <radisa@gmail.com>
2022-01-03 21:48:29 +0100

    initial version of 10.3.22-1ubuntu1

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,9 @@ RUN set -e; \
     apt-get dist-upgrade -y; \
     apt-get install -y --no-install-recommends \
       tini \
-      mariadb-server \
-      mariadb-client; \
+      crudini \
+      mariadb-server-10.3=1:10.3.22-1ubuntu1 \
+      mariadb-client-10.3=1:10.3.22-1ubuntu1; \
     apt-get autoremove -y; \
     apt-get clean; \
     rm -rvf /var/lib/apt/lists/*
@@ -20,22 +21,22 @@ RUN set -e; \
     mkdir -v /var/run/mysqld; \
     chown -v -R mysql:mysql /var/run/mysqld
 
-ENV MYSQL_SERVER_LOG_WARNINGS="4"
-ENV MYSQL_SERVER_GENERAL_LOG="0"
-ENV MYSQL_SERVER_SLOW_QUERY_LOG="1"
-ENV MYSQL_SERVER_LONG_QUERY_TIME="2"
-ENV MYSQL_SERVER_LOG_SLOW_RATE_LIMIT="1"
-ENV MYSQL_SERVER_LOG_SLOW_VERBOSITY="query_plan,explain,innodb"
-ENV MYSQL_SERVER_LOG_QUERIES_NOT_USING_INDEXES="1"
-ENV MYSQL_SERVER_LOG_SLOW_ADMIN_STATEMENTS="1"
-ENV MYSQL_SERVER_LOG_SLOW_FILTER=""
+# ENV MYSQL_SERVER_LOG_WARNINGS="4"
+# ENV MYSQL_SERVER_GENERAL_LOG="0"
+# ENV MYSQL_SERVER_SLOW_QUERY_LOG="1"
+# ENV MYSQL_SERVER_LONG_QUERY_TIME="2"
+# ENV MYSQL_SERVER_LOG_SLOW_RATE_LIMIT="1"
+# ENV MYSQL_SERVER_LOG_SLOW_VERBOSITY="query_plan,explain,innodb"
+# ENV MYSQL_SERVER_LOG_QUERIES_NOT_USING_INDEXES="1"
+# ENV MYSQL_SERVER_LOG_SLOW_ADMIN_STATEMENTS="1"
+# ENV MYSQL_SERVER_LOG_SLOW_FILTER=""
 
 VOLUME [ "/var/lib/mysql" ]
 
 EXPOSE 3306
 
 COPY 50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
-RUN chmod -v 0755 /etc/mysql/mariadb.conf.d/50-server.cnf
+RUN chmod -v 0644 /etc/mysql/mariadb.conf.d/50-server.cnf
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod -v 0755 /docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,27 +2,18 @@
 
 set -xe
 
+# TODO: implement supervisord for running 3 tails and mariadb
 tail -F /var/log/mysql/mysql.log >/dev/stdout &
 tail -F  /var/log/mysql/error.log >/dev/stderr &
 tail -F  /var/log/mysql/mariadb-slow.log >/dev/stdout &
 
-sed -i -e "s/^log_warnings\s.*$/log_warnings = ${MYSQL_SERVER_LOG_WARNINGS}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-sed -i -e "s/^general_log\s.*$/general_log = ${MYSQL_SERVER_GENERAL_LOG}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-sed -i -e "s/^slow_query_log\s.*$/slow_query_log = ${MYSQL_SERVER_SLOW_QUERY_LOG}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^long_query_time\s.*$/long_query_time = ${MYSQL_SERVER_LONG_QUERY_TIME}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^log_slow_rate_limit\s.*$/log_slow_rate_limit = ${MYSQL_SERVER_LOG_SLOW_RATE_LIMIT}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^log_slow_verbosity\s.*$/log_slow_verbosity = ${MYSQL_SERVER_LOG_SLOW_VERBOSITY}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^log_queries_not_using_indexes\s.*$/log_queries_not_using_indexes = ${MYSQL_SERVER_LOG_QUERIES_NOT_USING_INDEXES}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^log_slow_admin_statements\s.*$/log_slow_admin_statements = ${MYSQL_SERVER_LOG_SLOW_ADMIN_STATEMENTS}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^log_slow_filter\s.*$/log_slow_filter = ${MYSQL_SERVER_LOG_SLOW_FILTER}/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# DOCKER_RUN_AS_GID=$(stat -c '%g' /var/lib/mysql)
-# DOCKER_RUN_AS_UID=$(stat -c '%u' /var/lib/mysql)
-# groupmod -g ${DOCKER_RUN_AS_GID} mysql
-# usermod -u ${DOCKER_RUN_AS_UID} -g ${DOCKER_RUN_AS_GID} mysql
-# chown -vR mysql /var/log/mysql
+for ENVVAR in $(env | grep -E '^MARIADB_SERVER_CONF_.+')
+do
+  ENVVAR_SECTION=$(echo ${ENVVAR} | cut -d '=' -f2 | cut -d ':' -f 1)
+  ENVVAR_KEY=$(echo ${ENVVAR} | cut -d '=' -f2 | cut -d ':' -f 2)
+  ENVVAR_VALUE=$(echo ${ENVVAR} | cut -d '=' -f2 | cut -d ':' -f 3-)
+  crudini --verbose --set "/etc/mysql/mariadb.conf.d/50-server.cnf" "${ENVVAR_SECTION}" "${ENVVAR_KEY}" "${ENVVAR_VALUE}"
+done
 
 mysqld_safe &
 MYSQLD_SAFE_PID=$!
